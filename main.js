04850eea707bd786647f96e3ef36eb4f5b847eb3
Alvin <alvincheong@u.nus.edu>
2020-12-12 15:41:31 +0800

    done

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const SQL_SELECT_ALL_FROM_LISTS = 'select * from lists;'
 const SQL_SELECT_ALL_FROM_TASKS_WHERE_LISTID = 'select * from tasks where listID = ?;'
 const SQL_SELECT_COUNT_ALL_FROM_TASKS_WHERE_LISTID = 'select count(*) from tasks where listID = ?;'
 
-const SQL_ADD_NEW_LIST = 'insert into lists (listName, taskCount, digitalOceanKey) values (?,?, ?);'
+const SQL_ADD_NEW_LIST = 'insert into lists (listName, taskCount) values (?,?);'
 const SQL_ADD_NEW_TASK = 'insert into tasks (taskName, listID) values (?,?);'
 
 const SQL_DELETE_ID_FROM_LISTS = 'delete from lists where listID = ?;'
@@ -71,3 +71,180 @@ app.get('/lists', async (req, resp) => {
 		conn.release()
 	}
 })
+
+app.get('/tasks/:listID', async (req, resp) => {
+	const conn = await pool.getConnection()
+	try {
+		const [ result, _ ] = await conn.query(SQL_SELECT_ALL_FROM_TASKS_WHERE_LISTID, [req.params.listID])
+
+		resp.status(200)
+		resp.type('application/json').send(result)
+	} catch(e) {
+		console.error('ERROR: ', e)
+		resp.status(500)
+		resp.end()
+	} finally {
+		conn.release()
+	}
+})
+
+app.post('/addTask', async (req, resp) => {
+
+    const taskName = req.body.taskName;
+    const listID = req.body.listID;
+
+	const conn = await pool.getConnection()
+	try {
+
+		await conn.beginTransaction() // to prevent only one DB from being updated
+
+        await conn.query(
+        	SQL_ADD_NEW_TASK, [taskName, listID],
+		)
+
+		// update tasks count in list
+		const [ count, _2 ] = await conn.query(SQL_SELECT_COUNT_ALL_FROM_TASKS_WHERE_LISTID, [listID])
+		await conn.query(SQL_UPDATE_COUNT_IN_LISTS, [count[0]['count(*)'], listID])
+
+
+		await conn.commit()
+
+		resp.status(200)
+		// resp.format({
+		// 	html: () => { resp.send('Thank you'); },
+		// 	json: () => { resp.json({status: 'ok'});}
+
+		// })
+		resp.json()
+
+	} catch(e) {
+		conn.rollback()
+		resp.status(500).send(e)
+		resp.end()
+	} finally {
+		conn.release()
+	}
+});
+
+app.post('/addList', async (req, resp) => {
+
+    const listName = req.body.listName;
+
+	const conn = await pool.getConnection()
+	try {
+
+		await conn.beginTransaction() // to prevent only one DB from being updated
+
+        await conn.query(
+            SQL_ADD_NEW_LIST, [listName, 0],
+		)
+
+		await conn.commit()
+
+		resp.status(200)
+		// resp.format({
+		// 	html: () => { resp.send('Thank you'); },
+		// 	json: () => { resp.json({status: 'ok'});}
+
+		// })
+		resp.json()
+
+	} catch(e) {
+		conn.rollback()
+		resp.status(500).send(e)
+		resp.end()
+	} finally {
+		conn.release()
+	}
+});
+
+app.post('/deleteList', async (req, resp) => {
+
+	const listID = req.body.listID;
+
+	const conn = await pool.getConnection()
+	try {
+
+		await conn.beginTransaction() // to prevent one table from being updated and not the other
+
+		await conn.query(SQL_DELETE_FROM_TASKS_WHERE_LISTID, [listID])
+		await conn.query(SQL_DELETE_ID_FROM_LISTS, [listID])
+
+		await conn.commit()
+
+		resp.status(200)
+		resp.type('applcation/json')
+		resp.json()
+        
+	} catch(e) {
+		conn.rollback()
+
+		console.error('ERROR: ', e)
+		resp.status(500)
+		resp.end()
+	} finally {
+		conn.release()
+	}
+})
+
+app.post('/deleteTask', async (req, resp) => {
+
+	const taskID = req.body.taskID;
+	const listID = req.body.listID;
+
+	const conn = await pool.getConnection()
+	try {
+
+		await conn.beginTransaction() // to prevent one table from being updated and not the other
+
+		await conn.query(SQL_DELETE_ID_FROM_TASKS, [taskID])
+
+		// update tasks count in list
+		const [ count, _2 ] = await conn.query(SQL_SELECT_COUNT_ALL_FROM_TASKS_WHERE_LISTID, [listID])
+		
+		conn.query(SQL_UPDATE_COUNT_IN_LISTS, [count[0]['count(*)'], listID])
+
+		conn.commit()
+
+		resp.status(200)
+		resp.type('applcation/json')
+		resp.json()
+        
+	} catch(e) {
+		conn.rollback()
+
+		console.error('ERROR: ', e)
+		resp.status(500)
+		resp.end()
+	} finally {
+		conn.release()
+	}
+})
+
+app.post('/editListName', async (req, resp) => {
+
+    const listName = req.body.listName;
+    const listID = req.body.listID;
+
+	const conn = await pool.getConnection()
+	try {
+
+		await conn.beginTransaction() // to prevent only one DB from being updated
+
+        await conn.query(
+            SQL_EDIT_LIST_NAME, [listName, listID],
+		)
+		await conn.commit()
+
+		resp.status(200)
+		resp.json()
+
+	} catch(e) {
+		conn.rollback()
+		resp.status(500).send(e)
+		resp.end()
+	} finally {
+		conn.release()
+	}
+});
+
